bae91bca7c4883fe96fb78f9441d8f26707a3acb
mgiorgio <matias.giorgio@gmail.com>
2014-02-12 13:48:45 -0800

    Rechecking ambiguous students two times

--- src/main/java/edu/uci/ics/attendance/CheckAttendance.java
+++ src/main/java/edu/uci/ics/attendance/CheckAttendance.java
@@ -6,7 +6,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -24,6 +26,12 @@ public class CheckAttendance {
 	private List<String> notecards;
 
 	public CheckAttendance() {
+	}
+
+	public static void main(String[] args) throws IOException {
+		CheckAttendance attendance = new CheckAttendance();
+
+		attendance.check();
 	}
 
 	public void check() throws IOException {
@@ -47,25 +55,57 @@ public class CheckAttendance {
 	}
 
 	private void updateStudentsAttendance() {
-		for (final String notecard : notecards) {
-			Collection matchingStudents = CollectionUtils.select(studentIDs.keySet(), new Predicate() {
+		List<String> ambiguous = checkPartialNumbers(notecards);
+		System.out.println("First pass:");
+		printAmbiguous(ambiguous);
+		ambiguous = checkPartialNumbers(ambiguous);
+		System.out.println("Second pass:");
+		printAmbiguous(ambiguous);
+	}
 
-				@Override
-				public boolean evaluate(Object object) {
-					String student = (String) object;
-					return student.contains(notecard);
-				}
-			});
+	private void printAmbiguous(List<String> ambiguous) {
+		for (String each : ambiguous) {
+			StringBuilder builder = new StringBuilder();
+			builder.append(each).append(": ");
+			for (Iterator iterator = CollectionUtils.select(this.studentIDs.entrySet(), new PartialNumberPredicate(each)).iterator(); iterator.hasNext();) {
+				Entry<String, Integer> student = (Entry<String, Integer>) iterator.next();
+
+				builder.append(student.getKey()).append(",");
+			}
+			System.out.println(builder.toString());
+		}
+	}
+
+	private class PartialNumberPredicate implements Predicate {
+
+		private String partialNumber;
+
+		public PartialNumberPredicate(String partialNumber) {
+			this.partialNumber = partialNumber;
+		}
+
+		@Override
+		public boolean evaluate(Object object) {
+			Entry<String, Integer> student = (Entry<String, Integer>) object;
+			return student.getKey().contains(this.partialNumber) && student.getValue() == 0;
+		}
+	}
+
+	private List<String> checkPartialNumbers(List<String> partialNumbers) {
+		List<String> ambiguous = new LinkedList<>();
+		for (final String notecard : partialNumbers) {
+			Collection matchingStudents = CollectionUtils.select(studentIDs.entrySet(), new PartialNumberPredicate(notecard));
 
 			if (matchingStudents.isEmpty()) {
 				System.out.println("EMPTY: " + notecard);
 			} else if (matchingStudents.size() > 1) {
-				System.out.println("AMBIGUOUS: " + notecard);
+				ambiguous.add(notecard);
 			} else {
-				String studentID = matchingStudents.iterator().next().toString();
+				String studentID = ((Entry<String, Integer>) matchingStudents.iterator().next()).getKey();
 				studentIDs.put(studentID, studentIDs.get(studentID) + 1);
 			}
 		}
+		return ambiguous;
 	}
 
 	private void loadNotecards() throws IOException {
@@ -73,7 +113,7 @@ public class CheckAttendance {
 	}
 
 	private void loadStudentIDList() throws FileNotFoundException, IOException {
-		studentIDs = new HashMap<>();
+		studentIDs = new LinkedHashMap<>();
 		CSVReader reader = new CSVReader(new FileReader("students.csv"));
 
 		List<String[]> all = reader.readAll();
